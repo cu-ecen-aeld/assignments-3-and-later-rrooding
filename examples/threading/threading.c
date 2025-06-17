3d49b51dee047e166ebaababa58f264686a672c6
Ralph Rooding <ralph@ralphrooding.com>
2025-06-17 08:54:04 +0200

    implemented assignment 4 threading

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,8 +10,26 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data *thread_data = (struct thread_data *)thread_param;
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
+    // TODO: wait,
+    int ret = usleep(thread_data->wait_to_obtain_ms);
+    if (ret == -1)
+        ERROR_LOG("failed to sleep before obtaining mutex");
+
+    // obtain mutex,
+    pthread_mutex_lock(thread_data->mutex);
+    
+    // wait,
+    usleep(thread_data->wait_to_release_ms);
+
+    // release mutex as described by thread_data structure
+    pthread_mutex_unlock(thread_data->mutex);
+
+
+    // Set thread_complete_success to true after thread completes
+    thread_data->thread_complete_success = true;
+    
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
     return thread_param;
@@ -23,11 +41,29 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
     /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
      * using threadfunc() as entry point.
-     *
+     */
+
+    struct thread_data* thread_data = (struct thread_data*) malloc(sizeof(struct thread_data));
+    if (thread_data == NULL) {
+        ERROR_LOG("Failed to allocate memory for thread_data");
+        return false;
+    }
+    thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data->wait_to_release_ms = wait_to_release_ms;
+    thread_data->thread_complete_success = false;
+    thread_data->mutex = mutex;
+
+    if (pthread_create(thread, NULL, threadfunc, (void *)thread_data) != 0) {
+        ERROR_LOG("Failed to create thread");
+        free(thread_data);
+        return false;
+    }
+
+    /**
      * return true if successful.
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    return true;
 }
 
